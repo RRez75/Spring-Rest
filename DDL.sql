62fdcc8fbdb3d295771b5df0e9bae216c8c2cdce
RafaelRez75 <rrezende75@gmail.com>
2020-09-15 13:30:21 +0000

    commit spring aula 1

--- DDL.sql
+++ DDL.sql
@@ -1,14 +1,30 @@
-create schema springboot;
+create schema anotacao;
 
-use springboot;
+use anotacao;
 
-create user 'root'@'localhost' identified by 'wMKxUp6f3FRJFNYbWFJv';
+create user 'user'@'localhost' identified by 'pass123';
 
-grant select, insert, delete, update on springboot.* to root@'localhost';
+grant select, insert, delete, update on anotacao.* to user@'localhost';
 
-create table alunos (
+create table usr_usuario (
   usr_id bigint unsigned not null auto_increment,
   usr_nome varchar(20) not null,
   usr_senha varchar(50) not null,
-  primary key (usr_id)
+  primary key (usr_id),
+  unique key uni_usuario_nome (usr_nome)
+);
+
+create table aut_autorizacao (
+  aut_id bigint unsigned not null auto_increment,
+  aut_nome varchar(20) not null,
+  primary key (aut_id),
+  unique key uni_aut_nome (aut_nome)
+);
+
+create table uau_usuario_autorizacao (
+  usr_id bigint unsigned not null,
+  aut_id bigint unsigned not null,
+  primary key (usr_id, aut_id),
+  foreign key aut_usuario_fk (usr_id) references usr_usuario (usr_id) on delete restrict on update cascade,
+  foreign key aut_autorizacao_fk (aut_id) references aut_autorizacao (aut_id) on delete restrict on update cascade
 );
--- src/test/java/br/gov/sp/fatec/springbootapp/SpringBootAppApplicationTests.java
+++ src/test/java/br/gov/sp/fatec/springbootapp/SpringBootAppApplicationTests.java
@@ -1,13 +1,33 @@
 package br.gov.sp.fatec.springbootapp;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+import br.gov.sp.fatec.springbootapp.entity.Usuario;
+import br.gov.sp.fatec.springbootapp.repository.UsuarioRepository;
 
 @SpringBootTest
+//sem nenhuma gravacao usamos transactional e rollback
+@Transactional
+@Rollback
+
 class SpringBootAppApplicationTests {
-
-	@Test
+    
+    @Autowired
+    private UsuarioRepository usuarioRepo;
+    
+    @Test
 	void contextLoads() {
-	}
+    }
+    @Test
+    void testaInsercao(){
+        Usuario usuario = new  Usuario();
+        usuario.setNome("Rafael");
+        usuario.setSenha("12345");
+        usuarioRepo.save(usuario);
+        assertNotNull(usuario.getId());
 
-}
+    }}
