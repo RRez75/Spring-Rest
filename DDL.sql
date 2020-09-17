ea7812d46fb2a02065544716e16917d134051ef9
RafaelRez75 <rrezende75@gmail.com>
2020-09-17 13:17:13 +0000

    Spring Data JPA 2 (Erro no minuto 18)

--- DDL.sql
+++ DDL.sql
@@ -28,3 +28,10 @@ create table uau_usuario_autorizacao (
   foreign key aut_usuario_fk (usr_id) references usr_usuario (usr_id) on delete restrict on update cascade,
   foreign key aut_autorizacao_fk (aut_id) references aut_autorizacao (aut_id) on delete restrict on update cascade
 );
+
+insert into usr_usuario (usr_nome, usr_senha)
+    values ('Rafael','12345');
+insert into aut_autorizacao(aut_nome)
+    values ('ROLE_ADMIN');
+insert into uau_usuario_autorizacao 
+    values (7, 1);
--- src/test/java/br/gov/sp/fatec/springbootapp/SpringBootAppApplicationTests.java
+++ src/test/java/br/gov/sp/fatec/springbootapp/SpringBootAppApplicationTests.java
@@ -4,8 +4,8 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.test.annotation.Rollback;
 import br.gov.sp.fatec.springbootapp.entity.Usuario;
 import br.gov.sp.fatec.springbootapp.repository.UsuarioRepository;
 
@@ -25,9 +25,16 @@ class SpringBootAppApplicationTests {
     @Test
     void testaInsercao(){
         Usuario usuario = new  Usuario();
-        usuario.setNome("Rafael");
+        usuario.setNome("Rafaelo");
         usuario.setSenha("12345");
         usuarioRepo.save(usuario);
         assertNotNull(usuario.getId());
+    }
 
-    }}
+    @Test
+    void testaAutorizacao(){
+        Usuario usuario = usuarioRepo.findById(1L).get();
+        assertEquals("ROLE_ADMIN", usuario.getAutorizacoes().iterator().next().getNome());
+    }
+    
+}
