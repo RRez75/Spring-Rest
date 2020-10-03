f0aed9205f4bd5e308d6132542748b8556d023ac
RafaelRez75 <rrezende75@gmail.com>
2020-10-03 21:28:34 +0000

    Aula Rest com Spring 2

--- src/main/java/br/gov/sp/fatec/springbootapp/controller/UsuarioController.java
+++ src/main/java/br/gov/sp/fatec/springbootapp/controller/UsuarioController.java
@@ -1,6 +1,10 @@
 package br.gov.sp.fatec.springbootapp.controller;
 
 import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonView;
+
+import br.gov.sp.fatec.springbootapp.entity.Autorizacao;
 import br.gov.sp.fatec.springbootapp.entity.Usuario;
 import br.gov.sp.fatec.springbootapp.service.SegurancaService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,16 +25,19 @@ public class UsuarioController {
     @Autowired
     private SegurancaService segurancaService;
 
+    @JsonView(View.UsuarioResumo.class)
     @GetMapping
     public List<Usuario> buscarTodos(){
         return segurancaService.buscarTodosUsuarios();
     }
 
+    @JsonView(View.UsuarioCompleto.class)
     @GetMapping(value = "/{id}")
     public Usuario buscarPorId(@PathVariable("id") Long id){
         return segurancaService.buscarUsuarioPorId(id);
     }
 
+    @JsonView(View.UsuarioResumo.class)
     @GetMapping(value = "/nome")
     public Usuario buscarPorNome(@RequestParam(value="nome") String nome){
         return segurancaService.buscarUsuarioPorNome(nome);
@@ -39,5 +46,12 @@ public class UsuarioController {
     @PostMapping
     public Usuario cadastraNovoUsuario(@RequestBody Usuario usuario){
         return segurancaService.criarUsuario(usuario.getNome(), usuario.getSenha(), "base64 image", "ROLE_USUARIO");
+    }
+
+    @JsonView(View.AutorizacaoResumo.class)
+    @GetMapping(value = "/autorizacao/{autorizacao}")
+    public Autorizacao buscarAutorizacaoPorNome(@PathVariable("autorizacao") String nome){
+        return segurancaService.buscarAutorizacaoPorNome(nome);
+        
     }
 }
