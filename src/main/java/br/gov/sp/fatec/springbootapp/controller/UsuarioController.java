f069bf596e2127cb956efb315ee0f0ce4da946ac
RafaelRez75 <rrezende75@gmail.com>
2020-10-04 14:06:37 +0000

    Aula Rest com Spring 3

--- src/main/java/br/gov/sp/fatec/springbootapp/controller/UsuarioController.java
+++ src/main/java/br/gov/sp/fatec/springbootapp/controller/UsuarioController.java
@@ -8,11 +8,15 @@ import br.gov.sp.fatec.springbootapp.entity.Autorizacao;
 import br.gov.sp.fatec.springbootapp.entity.Usuario;
 import br.gov.sp.fatec.springbootapp.service.SegurancaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -44,9 +48,16 @@ public class UsuarioController {
     }
 
     @PostMapping
-    public Usuario cadastraNovoUsuario(@RequestBody Usuario usuario){
-        return segurancaService.criarUsuario(usuario.getNome(), usuario.getSenha(), "base64 image", "ROLE_USUARIO");
-    }
+    public ResponseEntity<Usuario> cadastraNovoUsuario(@RequestBody Usuario usuario,
+        UriComponentsBuilder uriComponentsBuilder){
+          usuario = segurancaService.criarUsuario(usuario.getNome(), usuario.getSenha(), "base64 image", "ROLE_USUARIO");
+                HttpHeaders responseHeaders = new HttpHeaders();
+                responseHeaders.setLocation(
+                    uriComponentsBuilder.path(
+                        "/usuario/" + usuario.getId()).build().toUri()
+                );
+                return new ResponseEntity<Usuario>(usuario, responseHeaders, HttpStatus.CREATED);
+        }
 
     @JsonView(View.AutorizacaoResumo.class)
     @GetMapping(value = "/autorizacao/{autorizacao}")
